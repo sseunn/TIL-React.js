243839b6b71efd0784db14af58801092c8a6bf5a
김선주 <sseunn123@naver.com>
2022-05-23 18:43:13 +0900

    Add: useState변경

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let post = "강남우동 맛집";
-  let [a, b] = useState("남자 코트 추천");
+  // let post = "강남우동 맛집";
+  // let posts = "파이썬 독학";
+  let [글제목, b] = useState("남자 코트 추천");
+  let [글제목2, c] = useState("강남우동 맛집");
+  let [글제목3, d] = useState("파이썬 독학");
+  let [logo, setLogo] = useState("React Blog");
+  let [date, setDate] = useState("2월 17일 발행");
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4> 블로그임</h4>
+        <h4> {logo}</h4>
       </div>
       <div className="list">
-        <h4>{a}</h4>
-        <p>2월 17일 발행</p>
+        <h4>{글제목}</h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{글제목3}</h4>
+        <p>{date}</p>
       </div>
     </div>
   );
