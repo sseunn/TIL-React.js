c03bc466281ee797646aac3173c66af9ca9ef33d
김선주 <sseunn123@naver.com>
2022-05-24 13:50:11 +0900

    state변경함수작성

--- src/App.js
+++ src/App.js
@@ -4,27 +4,45 @@ import { useState } from "react";
 function App() {
   // let post = "강남우동 맛집";
   // let posts = "파이썬 독학";
-  let [글제목, b] = useState("남자 코트 추천");
-  let [글제목2, c] = useState("강남우동 맛집");
-  let [글제목3, d] = useState("파이썬 독학");
+  let [글제목, b] = useState([
+    "남자 코트 추천",
+    "강남우동 맛집",
+    "파이썬 독학",
+  ]);
+
   let [logo, setLogo] = useState("React Blog");
   let [date, setDate] = useState("2월 17일 발행");
+  let [따봉, 따봉변경] = useState(0);
+  function 함수() {
+    console.log(1);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4> {logo}</h4>
       </div>
+      <button onClick = { }> 글수정 </button>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(1);
+            }}
+          >
+            👍
+          </span>
+          {따봉}
+        </h4>
         <p>{date}</p>
       </div>
       <div className="list">
-        <h4>{글제목2}</h4>
+        <h4>{글제목[1]}</h4>
         <p>{date}</p>
       </div>
       <div className="list">
-        <h4>{글제목3}</h4>
+        <h4>{글제목[2]}</h4>
         <p>{date}</p>
       </div>
     </div>
